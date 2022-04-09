8c4c061e54dbaee59e1558f49438d61c8cc2ff02
Benjamin Hoffner-Brodsky <benjaminhoffnerbrodsky@college.harvard.edu>
2022-04-08 21:42:35 -0400

    splitting id

---
+++ glimpse.R
@@ -0,0 +1,5 @@
+library(tidyverse)
+data <- read_csv("diabetic_data_initial.csv")
+discharge_ids <- read_csv("discharge.csv")
+admission_type_ids <- read_csv("admission_type.csv")
+admission_source_ids <- read_csv("admission_type.csv")
